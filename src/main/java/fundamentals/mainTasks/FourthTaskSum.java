60290076e3d161c49c7a52432b5d9a518ee5a387
Virinskaya <e.virinskaya@gmail.com>
2021-10-07 19:55:08 +0300

    Version 3. Fixed tasks 2 and 4

--- src/main/java/fundamentals/mainTasks/FourthTaskSum.java
+++ src/main/java/fundamentals/mainTasks/FourthTaskSum.java
@@ -1,10 +1,17 @@
 package fundamentals.mainTasks;
 
+import java.util.Scanner;
+
 public class FourthTaskSum {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NumberFormatException {
+        Scanner scanner = new Scanner(System.in);
         int sum = 0;
-        for (String arg : args) {
-            sum += Integer.parseInt(arg);
+        String input = scanner.nextLine();
+
+        while (!input.equals("")) {
+            sum += Integer.parseInt(input);
+            input = scanner.nextLine();
+
         }
         System.out.println("Result: " + sum);
     }
--- src/main/java/fundamentals/mainTasks/SecondTaskReverse.java
+++ src/main/java/fundamentals/mainTasks/SecondTaskReverse.java
@@ -2,10 +2,8 @@ package fundamentals.mainTasks;
 
 public class SecondTaskReverse {
     public static void main(String[] args) {
-        int j = args.length - 1;
-        for (int i = 0; i < args.length; i++) {
-            System.out.print(args[j] + " ");
-            j--;
+        for (int i = args.length - 1; i >= 0; i--) {
+            System.out.print(args[i] + " ");
         }
     }
 }
